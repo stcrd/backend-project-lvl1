47e8bc66f8caddb6b82faafd485a2f0c19cf0979
stcrd <nurzhan82@mail.ru>
2020-11-18 13:21:04 +0600

    make the Object more concise

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -5,34 +5,9 @@ import * as progression from './games/progression.js';
 import * as prime from './games/prime.js';
 
 export default {
-  even: {
-    initialData: even.getInitialData,
-    parsedQuestion: even.getQuestion,
-    parsedAnswer: even.getAnswer,
-    message: even.getMessage(),
-  },
-  calculator: {
-    initialData: calculator.getInitialData,
-    parsedQuestion: calculator.getQuestion,
-    parsedAnswer: calculator.getAnswer,
-    message: calculator.getMessage(),
-  },
-  gcd: {
-    initialData: gcd.getInitialData,
-    parsedQuestion: gcd.getQuestion,
-    parsedAnswer: gcd.getAnswer,
-    message: gcd.getMessage(),
-  },
-  progression: {
-    initialData: progression.getInitialData,
-    parsedQuestion: progression.getQuestion,
-    parsedAnswer: progression.getAnswer,
-    message: progression.getMessage(),
-  },
-  prime: {
-    initialData: prime.getInitialData,
-    parsedQuestion: prime.getQuestion,
-    parsedAnswer: prime.getAnswer,
-    message: prime.getMessage(),
-  },
+  even,
+  calculator,
+  gcd,
+  progression,
+  prime,
 };
