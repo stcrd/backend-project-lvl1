d301368965f36906963fcc6b883de78e2f187e7b
stcrd <nurzhan82@mail.ru>
2020-11-18 13:09:42 +0600

    update dispatcher

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,38 +1,38 @@
-import { getEvenInitialData, getEvenParsedQuestion, getEvenParsedAnswer } from './games/even.js';
-import { getCalcInitialData, getCalcParsedQuestion, getCalcParsedAnswer } from './games/calculator.js';
-import { getGcdInitialData, getGcdParsedQuestion, getGcdParsedAnswer } from './games/gcd.js';
-import { getProgressionInitialData, getProgressionParsedQuestion, getProgressionParsedAnswer } from './games/progression.js';
-import { getPrimeInitialData, getPrimeParsedQuestion, getPrimeParsedAnswer } from './games/prime.js';
+import * as even from './games/even.js';
+import * as calculator from './games/calculator.js';
+import * as gcd from './games/gcd.js';
+import * as progression from './games/progression.js';
+import * as prime from './games/prime.js';
 
 export default {
   even: {
-    initialData: getEvenInitialData,
-    parsedQuestion: getEvenParsedQuestion,
-    parsedAnswer: getEvenParsedAnswer,
-    message: 'Answer "yes" if the number is even, otherwise answer "no".',
+    initialData: even.getInitialData,
+    parsedQuestion: even.getQuestion,
+    parsedAnswer: even.getAnswer,
+    message: even.getMessage(),
   },
   calculator: {
-    initialData: getCalcInitialData,
-    parsedQuestion: getCalcParsedQuestion,
-    parsedAnswer: getCalcParsedAnswer,
-    message: 'What is the result of the expression?',
+    initialData: calculator.getInitialData,
+    parsedQuestion: calculator.getQuestion,
+    parsedAnswer: calculator.getAnswer,
+    message: calculator.getMessage(),
   },
   gcd: {
-    initialData: getGcdInitialData,
-    parsedQuestion: getGcdParsedQuestion,
-    parsedAnswer: getGcdParsedAnswer,
-    message: 'Find the greatest common divisor of given numbers.',
+    initialData: gcd.getInitialData,
+    parsedQuestion: gcd.getQuestion,
+    parsedAnswer: gcd.getAnswer,
+    message: gcd.getMessage(),
   },
   progression: {
-    initialData: getProgressionInitialData,
-    parsedQuestion: getProgressionParsedQuestion,
-    parsedAnswer: getProgressionParsedAnswer,
-    message: 'What number is missing in the progression?',
+    initialData: progression.getInitialData,
+    parsedQuestion: progression.getQuestion,
+    parsedAnswer: progression.getAnswer,
+    message: progression.getMessage(),
   },
   prime: {
-    initialData: getPrimeInitialData,
-    parsedQuestion: getPrimeParsedQuestion,
-    parsedAnswer: getPrimeParsedAnswer,
-    message: 'Answer "yes" if given number is prime. Otherwise answer "no".',
+    initialData: prime.getInitialData,
+    parsedQuestion: prime.getQuestion,
+    parsedAnswer: prime.getAnswer,
+    message: prime.getMessage(),
   },
 };
