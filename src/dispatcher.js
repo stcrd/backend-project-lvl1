433b5f5d9f18310d3797ad5e704410fba599a199
stcrd <nurzhan82@mail.ru>
2020-11-18 11:26:15 +0600

    update dispatcher with progression section

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,6 +1,7 @@
 import { getEvenInitialData, getEvenParsedQuestion, getEvenParsedAnswer } from './games/even.js';
 import { getCalcInitialData, getCalcParsedQuestion, getCalcParsedAnswer } from './games/calculator.js';
 import { getGcdInitialData, getGcdParsedQuestion, getGcdParsedAnswer } from './games/gcd.js';
+import { getProgressionInitialData, getProgressionParsedQuestion, getProgressionParsedAnswer } from './games/progression.js';
 
 export default {
   even: {
@@ -20,5 +21,11 @@ export default {
     parsedQuestion: getGcdParsedQuestion,
     parsedAnswer: getGcdParsedAnswer,
     message: 'Find the greatest common divisor of given numbers.',
+  },
+  progression: {
+    initialData: getProgressionInitialData,
+    parsedQuestion: getProgressionParsedQuestion,
+    parsedAnswer: getProgressionParsedAnswer,
+    message: 'What number is missing in the progression?',
   },
 };
