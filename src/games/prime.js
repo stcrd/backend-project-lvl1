c84a71503ec677f82d2c9bc6191c4ab967a2889e
stcrd <nurzhan82@mail.ru>
2020-11-18 13:11:05 +0600

    update prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,6 +4,14 @@ const first20Primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 5
 const [maxNumber] = first20Primes.slice(-1);
 const isPrime = (num) => first20Primes.includes(num);
 
-export const getPrimeInitialData = () => getRandomInt(maxNumber);
-export const getPrimeParsedQuestion = (number) => `${number}`;
-export const getPrimeParsedAnswer = (number) => (isPrime(number) ? 'yes' : 'no');
+const getInitialData = () => getRandomInt(maxNumber);
+const getQuestion = (number) => `${number}`;
+const getAnswer = (number) => (isPrime(number) ? 'yes' : 'no');
+const getMessage = () => 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+export {
+  getInitialData,
+  getQuestion,
+  getAnswer,
+  getMessage,
+};
