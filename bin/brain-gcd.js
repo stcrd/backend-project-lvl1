fc605040814912bdcc58c6ec675f549c9f771ea0
stcrd <nurzhan82@mail.ru>
2020-11-24 22:53:09 +0600

    delete the param in function call in executables

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import main from '../src/index.js';
+import startGame from '../src/games/gcd.js';
 
-main('gcd');
+startGame();
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import main from '../src/index.js';
+import startGame from '../src/games/progression.js';
 
-main('progression');
+startGame();
