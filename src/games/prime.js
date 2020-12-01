4a7a4f6a20d2477a49b42d5b4b7812dc284aaead
stcrd <nurzhan82@mail.ru>
2020-12-01 15:46:18 +0600

    fix isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,18 +1,22 @@
-import getRandomInt from '../common-functions.js';
+import getRandomInt from '../generate-random-int.js';
 import main from '../index.js';
 
 const min = 3;
 const max = 100;
-const initialDivisor = 2;
-const isPrime = (num, divisor) => {
-  if (num % divisor === 0) return false;
-  if (divisor > Math.sqrt(num)) return true;
-  return isPrime(num, divisor + 1);
+
+const isPrime = (num) => {
+  const initialDivisor = 2;
+  const iter = (number, divisor) => {
+    if (number % divisor === 0) return false;
+    if (divisor > Math.sqrt(number)) return true;
+    return iter(number, divisor + 1);
+  };
+  return iter(num, initialDivisor);
 };
 
 const getInitialData = () => getRandomInt(min, max);
 const getQuestion = (number) => `${number}`;
-const getAnswer = (number) => (isPrime(number, initialDivisor) ? 'yes' : 'no');
+const getAnswer = (number) => (isPrime(number) ? 'yes' : 'no');
 const getInstruction = () => 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getData = () => {
