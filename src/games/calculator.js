4badd4588116dbceb6df2b8b4bc3319c984b0d85
stcrd <nurzhan82@mail.ru>
2020-12-03 02:21:51 +0600

    refactor calculator.js

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -1,56 +1,24 @@
-import getRandomInt from '../common-functions.js';
-import main from '../index.js';
+import getRandomInt from '../generate-random-int.js';
+import runGame from '../index.js';
 
-const symbols = {
-  add: '+',
-  subtract: '-',
-  multiply: '*',
+const operations = {
+  add: { symbol: '+', function: (num1, num2) => num1 + num2 },
+  subtract: { symbol: '-', function: (num1, num2) => num1 - num2 },
+  multiply: { symbol: '*', function: (num1, num2) => num1 * num2 },
 };
-const functions = {
-  add: (num1, num2) => num1 + num2,
-  subtract: (num1, num2) => num1 - num2,
-  multiply: (num1, num2) => num1 * num2,
-};
-const operations = ['add', 'subtract', 'multiply'];
-const numberOfOperations = operations.length;
-
+const numberOfOperations = Object.keys(operations).length;
 const maxNumber = 20;
-
-const getRandomOperation = () => {
-  const randomIndex = getRandomInt(0, numberOfOperations);
-  return operations[randomIndex];
-};
-
-const getInitialData = () => {
-  const num1 = getRandomInt(0, maxNumber);
-  const num2 = getRandomInt(0, maxNumber);
-  const operation = getRandomOperation();
-  const symbol = symbols[operation];
-  return {
-    num1,
-    num2,
-    symbol,
-    operation,
-  };
-};
-const getQuestion = ({ num1, num2, symbol }) => `${num1} ${symbol} ${num2}`;
-const getAnswer = ({ num1, num2, operation }) => {
-  const operationFunction = functions[operation];
-  const result = operationFunction(num1, num2);
-  return result.toString();
-};
-const getInstruction = () => 'What is the result of the expression?';
+const instruction = 'What is the result of the expression?';
 
 const getData = () => {
-  const data = getInitialData();
-  const question = getQuestion(data);
-  const answer = getAnswer(data);
-  const instruction = getInstruction();
-  return {
-    getData,
-    question,
-    answer,
-    instruction,
-  };
+  const randomIndex = getRandomInt(0, numberOfOperations);
+  const operation = Object.keys(operations)[randomIndex];
+  const { symbol } = operations[operation];
+  const num1 = getRandomInt(0, maxNumber);
+  const num2 = getRandomInt(0, maxNumber);
+  const question = `${num1} ${symbol} ${num2}`;
+  const answer = operations[operation].function(num1, num2).toString();
+  return { question, answer, instruction };
 };
-export default () => main(getData());
+
+export default () => runGame(getData);
