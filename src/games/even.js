a992550dd15a83f5e9963176e404ed401e9731d5
stcrd <nurzhan82@mail.ru>
2020-12-03 02:22:12 +0600

    refactor even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,24 +1,15 @@
-import getRandomInt from '../common-functions.js';
-import main from '../index.js';
+import getRandomInt from '../generate-random-int.js';
+import runGame from '../index.js';
 
 const isEven = (num) => num % 2 === 0;
 const maxNumber = 99;
-
-const getInitialData = () => getRandomInt(0, maxNumber);
-const getQuestion = (number) => `${number}`;
-const getAnswer = (number) => (isEven(number) ? 'yes' : 'no');
-const getInstruction = () => 'Answer "yes" if the number is even, otherwise answer "no".';
+const instruction = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getData = () => {
-  const data = getInitialData();
-  const question = getQuestion(data);
-  const answer = getAnswer(data);
-  const instruction = getInstruction();
-  return {
-    getData,
-    question,
-    answer,
-    instruction,
-  };
+  const num = getRandomInt(0, maxNumber);
+  const question = num.toString();
+  const answer = isEven(num) ? 'yes' : 'no';
+  return { question, answer, instruction };
 };
-export default () => main(getData());
+
+export default () => runGame(getData);
