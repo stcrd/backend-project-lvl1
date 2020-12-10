0798f8c8e6a4a847d23f441c7bc49bc777a8eb94
stcrd <nurzhan82@mail.ru>
2020-12-10 18:31:16 +0600

    fix executeGameRound parameters and several other minor issues

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 import readlineSync from 'readline-sync';
 
-const runGame = (generateNewRound, instruction) => {
+const runGame = (generateRound, instruction) => {
   const maxScore = 3;
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(instruction);
 
-  const executeGameRound = ({ question, answer }, score) => {
-    if (score === 0) {
+  const executeGameRound = (score) => {
+    if (score === maxScore) {
       console.log(`Congratulations, ${name}!`);
       return;
     }
+    const { question, answer } = generateRound();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === answer) {
       console.log('Correct!');
-      executeGameRound(generateNewRound(), score - 1);
+      executeGameRound(score + 1);
     } else {
-      console.log(`'${userAnswer}' is a wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${name}!`);
+      console.log(`'${userAnswer}' is a wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${name}!`);
     }
   };
-  executeGameRound(generateNewRound(), maxScore);
+  executeGameRound(0);
 };
 
 export default runGame;
