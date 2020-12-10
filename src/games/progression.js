ad0073dd7507d35247aa7bc005a3d066d524bb04
stcrd <nurzhan82@mail.ru>
2020-12-10 18:29:34 +0600

    fix length and count naming

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,26 +1,26 @@
 import getRandomInt from '../generate-random-int.js';
 import runGame from '../index.js';
 
-const maxLength = 10;
+const length = 10;
 const maxStartingNumber = 20;
 const maxIncrement = 5;
 const instruction = 'What number is missing in the progression?';
 
 const generateProgression = (first, increment) => {
-  const iter = (progression, length) => {
-    if (length === maxLength) return progression;
-    return iter([...progression, first + increment * length], length + 1);
+  const iter = (progression, count) => {
+    if (count === length) return progression;
+    return iter([...progression, first + increment * count], count + 1);
   };
   return iter([], 0);
 };
 
-const generateNewRound = () => {
+const generateRound = () => {
   const startingNumber = getRandomInt(0, maxStartingNumber);
   const increment = getRandomInt(1, maxIncrement);
-  const hiddenIndex = getRandomInt(0, maxLength - 1);
+  const hiddenIndex = getRandomInt(0, length - 1);
   const progression = generateProgression(startingNumber, increment);
   const question = [...progression].fill('..', hiddenIndex, hiddenIndex + 1).join(' ');
   const answer = (startingNumber + increment * hiddenIndex).toString();
   return { question, answer };
 };
-export default () => runGame(generateNewRound, instruction);
+export default () => runGame(generateRound, instruction);
