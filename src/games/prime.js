fca5c7067403935be873f1541f23ec53e5a03a08
stcrd <nurzhan82@mail.ru>
2020-12-10 20:10:02 +0600

    add condition for 2 in isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,6 +7,7 @@ const instruction = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (num) => {
   if (num === 1) return false;
+  if (num === 2) return true;
   const initialDivisor = 2;
   const iter = (divisor) => {
     if (num % divisor === 0) return false;
