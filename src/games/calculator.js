2edd4c5ed3f7b73cbd97a2a1aee0f633998b8eb3
stcrd <nurzhan82@mail.ru>
2020-12-10 18:24:03 +0600

    simplify operations object

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -2,9 +2,9 @@ import getRandomInt from '../generate-random-int.js';
 import runGame from '../index.js';
 
 const operations = {
-  add: { symbol: '+', function: (num1, num2) => num1 + num2 },
-  subtract: { symbol: '-', function: (num1, num2) => num1 - num2 },
-  multiply: { symbol: '*', function: (num1, num2) => num1 * num2 },
+  '+': (num1, num2) => num1 + num2,
+  '-': (num1, num2) => num1 - num2,
+  '*': (num1, num2) => num1 * num2,
 };
 const numberOfOperations = Object.keys(operations).length;
 const maxNumber = 20;
@@ -13,11 +13,10 @@ const instruction = 'What is the result of the expression?';
 const generateNewRound = () => {
   const randomIndex = getRandomInt(0, numberOfOperations);
   const operation = Object.keys(operations)[randomIndex];
-  const { symbol } = operations[operation];
   const num1 = getRandomInt(0, maxNumber);
   const num2 = getRandomInt(0, maxNumber);
-  const question = `${num1} ${symbol} ${num2}`;
-  const answer = operations[operation].function(num1, num2).toString();
+  const question = `${num1} ${operation} ${num2}`;
+  const answer = operations[operation](num1, num2).toString();
   return { question, answer };
 };
 
